d616d1c3653cbba41b84614db371ca47ee8c669f
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-11-09 13:49:39 -0600

    Update marketplace.sol

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -4,17 +4,33 @@ import "./buildingGeneration.sol"
 
 contract marketplace is buildingGeneration {
 
+        // Declare all events here
         event itemListed(uint item, uint quantity, uint price, address owner);
         event lotteryBegin(uint entryFee);
         event buildingListed(uint price, address owner);
 
+        // Declare all variables here
         uint private winningIndex;
-        
 
-        //oracle required
-        function _randomIndex() internal view returns (uint) {
+        // Declare all modifiers
+        modifier enoughResources(address _address, uint16 _biome, uint8 _rarity, address _amount) {
+                uint counter;
+                for(uint i = 0; i < resources.length; i++) {
+                        if(resources[i].biome == _biome && resources[i].rarity == _rarity) {
+                                counter++;
+                        }
+                }
+                require(counter >= _amount);
+                _;
         }
 
-        function listItem(uint _item, uint _quantity, uint _price, address _owner) public {
-               require (
+        // Declare all functions here
+
+        function _randomIndex() internal view returns (uint) {
+        // TODO: use an oracle to get a random number
+        }
+
+        function listItem(uint _quantity, uint _price, uint16 _biome, uint8 _rarity, address _owner) public enoughResources(_owner, _biome, _rarity, _quantity) {
+        // TODO: Puts the item in escrow, where a person can pay the address _price tokens and receive _item
+        }
 }
