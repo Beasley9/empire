0c7798625d2eec80be8f74c8af595809b2dc821e
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-11-10 16:21:06 -0600

    Update buildingGeneration.sol

--- contracts/buildingGeneration.sol
+++ contracts/buildingGeneration.sol
@@ -2,9 +2,11 @@ pragma solidity >0.8.0;
 
 import "@openzepplin/contracts/tokens/ERC20/ERC20.sol";
 import "@openzepplin/contracts/tokens/ERC721/ERC721.sol";
-import "./spawnEvent.sol";
+import "@openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
 
 contract buildingGeneration is ERC20, ERC721, spawnEvent {
+
+        using SafeMath for uint256;
 
         // Declare global structs in this block
         struct internal building {
@@ -40,6 +42,9 @@ contract buildingGeneration is ERC20, ERC721, spawnEvent {
         // Order all global variables
         uint private cooldownTime;
         uint private genesisPeriod
+        uint internal spawnFrequency;
+        uint internal fortSpawnRarity;
+        uint internal strongholdSpawnRarity;
 
         // Order all modifier functions
         modifier outpostGenerationCooldownTime(address _user) {
@@ -98,4 +103,42 @@ contract buildingGeneration is ERC20, ERC721, spawnEvent {
         }
         function generateGenesisOutpost(address _user) public outpostGenerationCooldownTime(_user) isGenesis isOwner {
                 // Function will be the same as previous, except with more powerful maxEfficiency
+        }
+
+
+        // Spawn Events
+        function _getRandomSpawnIndex(uint _max) private view returns (uint) {
+                // TODO: use an oracle to find a random number between 1 and _max;
+        }
+
+        function setSpawnFrequency(uint _newSpawnFrequency) public onlyOwner {
+                spawnFrequency = _newSpawnFrequency;
+        }
+
+        function setFortSpawnRarity(uint _newFortSpawnRarity) public onlyOwner {
+                fortSpawnRarity = _newFortSpawnRarity;
+        }
+
+        function setStrongholdSpawnRarity(uint _newStrongholdSpawnRarity) public onlyOwner {
+                strongholdSpawnRarity = _newStrongholdSpawnRarity;
+        }
+
+        function getSpawnFrequency() external view returns (uint) {
+                return spawnFrequency;
+        }
+
+        function getFortSpawnRarity() external view returns (uint) {
+                return fortSpawnRarity;
+        }
+
+        function getStrongholdSpawnRarity() external view returns (uint) {
+                return strongholdSpawnRarity();
+        }
+
+        function _spawnEvent() internal {
+                uint spawnIndex = getRandomSpawnIndex(Buildings.length);
+        }
+
+        function _tokenBurn() internal {
+        }
 }
