8fe794a86d32007a997dc4767e0cec2451a10dc8
Beasley9 <57361391+Beasley9@users.noreply.github.com>
2021-11-10 16:42:40 -0600

    Update resourceGeneration.sol

--- contracts/resourceGeneration.sol
+++ contracts/resourceGeneration.sol
@@ -5,7 +5,7 @@ import "./buildingGeneration.sol"
 contract resourceGeneration is marketplace {
 
         // Declare all variables here
-
+        uint internal empireScore;
         // Declare all modifiers here 
 
         // Declare all constructors here
@@ -13,29 +13,33 @@ contract resourceGeneration is marketplace {
         // Declare all functions here
 
         // TODO: ensure that this function will produce an integer. UINTS CANNOT BE DECIMALS
-        function calculateEffectiveEfficiency(address _user, uint _buildingId) internal returns (uint) {
+        function calculateEffectiveEfficiency(address _user, building storage _building) internal returns (uint) {
                 uint effectiveEfficiency = 0;
-                if (buildingToOwner[_buildingId].upgradeLevel == 1) {
-                        effectiveEfficiency = uint((buildingToOwner[_buildingId].maxEfficiency/(1 + (1.1 ** (-2 * buildingToOwner[_buildingId].level)))));
+                if (_building.upgradeLevel == 1) {
+                        effectiveEfficiency = uint((_building.maxEfficiency/(1 + (1.1 ** (-2 * _building.level)))));
                 }
-                else if (buildingToOwner[_id].upgradeLevel == 2) {
-                        effectiveEfficiency = uint((buildingToOwner[_buildingId].maxEfficiency/(1 + (1.5 ** (-.5 * buildingToOwner[_buildingId].level)))));
+                else if (_building.upgradeLevel == 2) {
+                        effectiveEfficiency = uint((_building.maxEfficiency/(1 + (1.5 ** (-.5 * _building.level)))));
                 }
                 else {
-                        effectiveEfficiency = uint((buildingToOwner[_buildingId].maxEfficiency/(1 + (1.5 ** (-1 * buildingToOwner[_buildingId].level)))));
+                        effectiveEfficiency = uint((_building.maxEfficiency/(1 + (1.5 ** (-1 * _building.level)))));
                 }
-
                 return effectiveEfficiency;
         }
 
-        function collectResources(address _user, uint _buildingId) public {
-                uint efficiency = calculateEffectiveEfficiency(_user, _buildingId);
+        function collectResources(building storage _building) public {
+                require(buildingToOwner[_building] == msg.sender);
+                uint efficiency = calculateEffectiveEfficiency(_user, _building);
                 //TODO: Finish this function to add to the user's resource pool
         }
 
-        function collectAllResources(address _user) public isOwner(_user) {
-                for (uint i = 0; i < ownerNumBuildings[_user]; i++) {
+        function collectAllResources() public {
+                for (uint i = 0; i < ownerNumBuildings[msg.sender]; i++) {
                         collectResources(_user, i);
                 }
+        }
+
+        function collectEmpireTokens() public {
+                // TODO: write function for collecting stronghold tokens
         }
 }
